f16568975d8480e3238aa3282ee98eefbb000563
efilnukefesin <efil.threat@googlemail.com>
2023-12-30 23:07:51 +0100

    first commit

---
+++ src/SVRGN.Libs.Contracts.Service.Object/IObjectInfo.cs
@@ -0,0 +1,22 @@
+﻿using SVRGN.Libs.Contracts.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SVRGN.Libs.Contracts.Service.Object
+{
+    public interface IObjectInfo : IBaseObject
+    {
+        #region Properties
+
+        object Data { get; }
+        Type DataType { get; }
+        DateTime CreatedAt { get; }
+
+        #endregion Properties
+
+        #region Methods
+
+        #endregion Methods
+    }
+}
---
+++ src/SVRGN.Libs.Contracts.Service.Object/IObjectService.cs
@@ -0,0 +1,26 @@
+﻿using SVRGN.Libs.Contracts.Base;
+using SVRGN.Libs.Contracts.Service.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SVRGN.Libs.Contracts.Service.Object
+{
+    /// <summary>
+    /// the service responsible for objects and their creation
+    /// </summary>
+    public interface IObjectService : IService
+    {
+        #region Properties
+
+        #endregion Properties
+
+        #region Methods
+
+        T Create<T>(params object[] Parameters) where T : class;
+        T CreateWithTypeName<T>(string TypeName, params object[] Parameters) where T : class;
+        int Count();
+
+        #endregion Methods  
+    }
+}
